a149249158cca4a8673c84aeda91fcf038a6d117
Edin <edinvasquez412@gmail.com>
2020-06-04 13:11:41 -0500

    Agregue un programa de suma, suma.py

---
+++ suma.py
@@ -0,0 +1,7 @@
+import time
+print ("**** PROGRAMA DE SUMAS ****")
+Num1=int(input("Ingresar Primer Numero: "))
+Num2=int(input("Ingresar Segundo Numero:"))
+Sum=Num1+Num2
+print("La suma de los numeros es: ", Sum)
+time.sleep(10)
